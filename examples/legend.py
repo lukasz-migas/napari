0b7ad6219cb144f2d44e1560d0bfd7f9de3e56e6
Lukasz Migas <lukas.migas@yahoo.com>
2025-06-15 22:43:02 +0100

    Few fixes

--- examples/legend.py
+++ examples/legend.py
@@ -35,6 +35,7 @@ class LegendControls(QWidget):
         self.align_combo.addItems(
             ['row', 'column', 'row_split', 'column_split']
         )
+        self.align_combo.setCurrentText(viewer.legend_overlay.align.value)
         self.align_combo.currentTextChanged.connect(self.on_align_change)
 
         self.position_combo = QComboBox()
@@ -48,16 +49,19 @@ class LegendControls(QWidget):
                 'bottom_right',
             ]
         )
+        self.position_combo.setCurrentText(
+            viewer.legend_overlay.position.value
+        )
         self.position_combo.currentTextChanged.connect(self.on_position_change)
 
         self.font_size_spin = QSpinBox()
         self.font_size_spin.setRange(1, 100)
-        self.font_size_spin.setValue(24)
+        self.font_size_spin.setValue(int(viewer.legend_overlay.font_size))
         self.font_size_spin.setSingleStep(1)
         self.font_size_spin.valueChanged.connect(self.on_font_size_change)
 
         self.box_check = QCheckBox()
-        self.box_check.setChecked(True)
+        self.box_check.setChecked(viewer.legend_overlay.box)
         self.box_check.stateChanged.connect(self.on_box_changed)
 
         self.layout = QFormLayout(self)
@@ -84,8 +88,6 @@ class LegendControls(QWidget):
 
 
 viewer = napari.view_image(data.astronaut(), rgb=True)
-widget = LegendControls()
-viewer.window.add_dock_widget(widget, area='right')
 
 # Create a napari viewer with a 3D, 2 channel image, with the `view_image` convenience function.
 # viewer = napari.view_image(
@@ -94,13 +96,14 @@ viewer.window.add_dock_widget(widget, area='right')
 
 viewer.legend_overlay.visible = True
 viewer.legend_overlay.font_size = 24
-viewer.legend_overlay.position = 'bottom_left'
-viewer.legend_overlay.align = 'column'
+viewer.legend_overlay.position = 'bottom_center'
+viewer.legend_overlay.align = 'row'
 viewer.legend_overlay.box = True
 viewer.legend_overlay.box_color = 'yellow'
 viewer.legend_overlay.add('Red 1', 'red')
 viewer.legend_overlay.add('Green 1', 'green')
 viewer.legend_overlay.add('Blue 1', 'blue')
+viewer.legend_overlay.add('A much longer label', 'magenta')
 # viewer.legend_overlay.add('Red 2', 'red')
 # viewer.legend_overlay.add('Green 2', 'green')
 # viewer.legend_overlay.add('Blue 2', 'blue')
@@ -110,6 +113,10 @@ viewer.legend_overlay.add('Blue 1', 'blue')
 # viewer.legend_overlay.add('Red 4', 'red')
 # viewer.legend_overlay.add('Green 4', 'green')
 # viewer.legend_overlay.add('Blue 4', 'blue')
+
+# create widget
+widget = LegendControls()
+viewer.window.add_dock_widget(widget, area='right')
 
 if __name__ == '__main__':
     napari.run()
--- src/napari/_vispy/overlays/legend.py
+++ src/napari/_vispy/overlays/legend.py
@@ -111,7 +111,7 @@ def update_text(
     """Get position of the legend overlay."""
     from operator import add, sub
 
-    ovr = visual.overlay
+    ovr: LegendOverlay = visual.overlay
     if ovr.visible_count() == 0:
         visual.node.text.text = ovr.text()
         visual.node.text.color = ovr.color()
@@ -123,45 +123,64 @@ def update_text(
     offset_x = OFFSET_X_MAP[ovr.position]
     offset_y = OFFSET_Y_MAP[ovr.position]
 
-    max_x, max_y = visual.node.canvas.size
+    max_width, max_height = visual.node.canvas.size
     width = text_factor * 1.01
     height = text_factor * 1.75
 
     # let's calculate any offsets and anchors
-    start_x, start_y, x_operator, y_operator = (offset_x, offset_y, add, add)
-    if ovr.align in [A.COLUMN, A.COLUMN_SPLIT] and ovr.position == CP.TOP_LEFT:
+    start_x, start_y, x_operator, y_operator = offset_x, offset_y, add, add
+    is_row = ovr.align in [A.ROW, A.ROW_SPLIT]
+    is_col = ovr.align in [A.COLUMN, A.COLUMN_SPLIT]
+
+    # handle column alignment
+    if is_col and ovr.position == CP.TOP_LEFT:
         start_x, start_y = offset_x, offset_y
         x_operator = y_operator = add
-    elif (
-        ovr.align in [A.COLUMN, A.COLUMN_SPLIT]
-        and ovr.position == CP.TOP_RIGHT
-    ):
-        start_x, start_y = max_x, offset_y
+    elif is_col and ovr.position == CP.TOP_RIGHT:
+        start_x, start_y = max_width, offset_y
         x_operator, y_operator = sub, add
-    elif (
-        ovr.align in [A.COLUMN, A.COLUMN_SPLIT]
-        and ovr.position == CP.BOTTOM_LEFT
-    ):
-        start_x, start_y = offset_x, max_y - offset_y
+    elif is_col and ovr.position == CP.TOP_CENTER:
+        start_x = max_width / 2
+        start_y = offset_y
+        x_operator, y_operator = sub, add
+    elif is_col and ovr.position == CP.BOTTOM_LEFT:
+        start_x, start_y = offset_x, max_height - offset_y
         x_operator, y_operator = add, sub
-    elif (
-        ovr.align in [A.COLUMN, A.COLUMN_SPLIT]
-        and ovr.position == CP.BOTTOM_RIGHT
-    ):
-        start_x, start_y = max_x, max_y - offset_y
+    elif is_col and ovr.position == CP.BOTTOM_RIGHT:
+        start_x, start_y = max_width, max_height - offset_y
         x_operator, y_operator = sub, sub
-    elif ovr.align in [A.ROW, A.ROW_SPLIT] and ovr.position == CP.TOP_LEFT:
+    elif is_col and ovr.position == CP.BOTTOM_CENTER:
+        start_x = max_width / 2
+        start_y = max_height - offset_y
+        x_operator, y_operator = sub, sub
+    # handle row alignment
+    elif is_row and ovr.position == CP.TOP_LEFT:
         start_x, start_y = offset_x, offset_y + height
         x_operator = y_operator = add
-    elif ovr.align in [A.ROW, A.ROW_SPLIT] and ovr.position == CP.TOP_RIGHT:
-        start_x, start_y = max_x - offset_x, offset_y + height
+    elif is_row and ovr.position == CP.TOP_RIGHT:
+        start_x, start_y = max_width - offset_x, offset_y + height
         x_operator, y_operator = sub, add
-    elif ovr.align in [A.ROW, A.ROW_SPLIT] and ovr.position == CP.BOTTOM_LEFT:
-        start_x, start_y = offset_x, max_y - offset_y - height
+    elif is_row and ovr.position == CP.TOP_CENTER:
+        estimate_width, _ = _estimate_width_and_height(
+            ovr.align, ovr.text(), width, height, max_width
+        )
+        start_x = max_width / 2 - estimate_width / 2
+        start_y = offset_y + height
+        x_operator, y_operator = sub, add
+    elif is_row and ovr.position == CP.BOTTOM_LEFT:
+        start_x, start_y = offset_x, max_height - offset_y - height
         x_operator, y_operator = add, sub
-    elif ovr.align in [A.ROW, A.ROW_SPLIT] and ovr.position == CP.BOTTOM_RIGHT:
-        start_x, start_y = max_x - offset_x, max_y - offset_y - height
+    elif is_row and ovr.position == CP.BOTTOM_RIGHT:
+        start_x, start_y = max_width, max_height - offset_y - height
         x_operator, y_operator = sub, sub
+    elif is_row and ovr.position == CP.BOTTOM_CENTER:
+        estimate_width, estimate_height = _estimate_width_and_height(
+            ovr.align, ovr.text(), width, height, max_width
+        )
+        start_x = max_width / 2 - estimate_width / 2
+
+        start_y = max_height - offset_y - estimate_height
+        x_operator, y_operator = add, sub
 
     # let's keep the original start_x and start_y for the next row
     start_x_ = start_x
@@ -173,7 +192,7 @@ def update_text(
     # calculate the positions of the text
     texts = list(ovr.text())
     positions = np.empty((len(texts), 2), dtype=np.float32)
-    previous, padding = 0, 0
+    previous, padding = 0.0, 0
     for i, text in enumerate(texts):
         n = len(text)  # number of characters in the text
         padding = calculate_padding(text)
@@ -182,17 +201,17 @@ def update_text(
         # single row of legend items
         if ovr.align == A.ROW:
             start_x = x_operator(start_x, previous)
-            previous = n * width + padding
+            previous = _estimate_text_width(text, padding, width)
         # potentially multiple row of legend items
         elif ovr.align == A.ROW_SPLIT:
             start_x = x_operator(start_x, previous)
             if (
-                x_operator(start_x, previous) > max_x
+                x_operator(start_x, previous) > max_width
                 or x_operator(start_x, previous) < 0
             ):
                 start_x = x_operator(start_x_, 0)
                 start_y = y_operator(start_y, height)
-            previous = n * width + padding
+            previous = _estimate_text_width(text, padding, width)
 
         # single column of legend items
         elif ovr.align == A.COLUMN:
@@ -200,7 +219,7 @@ def update_text(
         # potentially multiple column of legend items
         elif ovr.align == A.COLUMN_SPLIT:
             start_y = y_operator(start_y, height)
-            if start_y < 0 or start_y + height > max_y:
+            if start_y < 0 or start_y + height > max_height:
                 start_y = y_operator(start_y_, height)
                 start_x = x_operator(
                     x_operator(start_x, max_text_width), width
@@ -217,31 +236,66 @@ def update_text(
     visual.node.text.pos = np.array(positions, dtype=np.float32)
     visual.node.text.anchors = (anchor_x, anchor_y)
 
-    positions = np.abs(positions)
-    mins = positions.min(axis=0)
-    maxs = positions.max(axis=0)
-    box_width, box_height = maxs - mins
-    # if splitting across row(s), center(x) should be (min + max) / 2 and center(y) should be
-    if ovr.align == A.ROW:
-        box_center_x = x_operator((mins[0] + maxs[0] - padding), previous) / 2
-        box_center_y = y_operator(
-            start_y_, (-height / 1.5 if anchor_y == 'top' else -height / 2)
-        )
-    elif ovr.align == A.COLUMN:
-        box_center_x = x_operator(start_x_, max_text_width / 2)
-        box_center_y = (
-            mins[1]
-            + maxs[1]
-            + (-height / 1.5 if anchor_y == 'top' else -height / 2)
-        ) / 2
-        box_height = box_height + height
-        box_width = max_text_width
-    box_center = (box_center_x, box_center_y)
-    box_width = box_width + previous or max_text_width
-    box_height = box_height or height
-    # print(positions, max_text_width)
-    # print('??', box_center, width, height)
-    return box_center, box_width, box_height
+    if ovr.box:
+        positions = np.abs(positions)
+        mins = positions.min(axis=0)
+        maxs = positions.max(axis=0)
+        box_width, box_height = maxs - mins
+        # if splitting across row(s), center(x) should be (min + max) / 2 and center(y) should be
+        if ovr.align == A.ROW:
+            box_center_x = (
+                x_operator((mins[0] + maxs[0] - padding), previous) / 2
+            )
+            box_center_y = y_operator(
+                start_y_, (-height / 1.5 if anchor_y == 'top' else -height / 2)
+            )
+        elif ovr.align == A.COLUMN:
+            box_center_x = x_operator(start_x_, max_text_width / 2)
+            box_center_y = (
+                mins[1]
+                + maxs[1]
+                + (-height / 1.5 if anchor_y == 'top' else -height / 2)
+            ) / 2
+            box_height = box_height + height
+            box_width = max_text_width
+
+        box_center = (box_center_x, box_center_y)
+        box_width = box_width + previous or max_text_width
+        box_height = box_height or height
+        # print(positions, max_text_width)
+        # print('??', box_center, width, height)
+        return box_center, box_width, box_height
+    return (0, 0), 1, 1
+
+
+def _estimate_text_width(text: str, padding: int, text_width: float) -> float:
+    """Estimate the width of the text."""
+    # This is a very rough estimate, as we don't have access to the actual font metrics.
+    # We assume an average character width of 7 pixels for normal characters,
+    # and 4 pixels for 'i' and 'l', and 10 pixels for 'm', 'w', and 'g'.
+    return len(text) * text_width + padding
+
+
+def _estimate_width_and_height(
+    alignment: A,
+    texts: list[str],
+    width: float,
+    height: float,
+    max_width: float,
+) -> tuple[float, float]:
+    """Estimate the width of the text based on the characters."""
+    if len(texts) == 0:
+        return 0, height
+    estimated_width = sum(
+        _estimate_text_width(text, calculate_padding(text), width)
+        for text in texts
+    )
+    if alignment == A.ROW:
+        return estimated_width, height
+    if alignment == A.ROW_SPLIT and estimated_width > max_width:
+        ratio = np.ceil(estimated_width / width)
+        return max_width, height * ratio
+    return 0, height
 
 
 def calculate_padding(text: str) -> int:
